1694183cf0fce46ef8b0f682cf4fe9547b144968
BasdatLimaX <andhikabrosnan@gmail.com>
2019-10-11 10:40:07 +0700

    first commit backup

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String abcUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abcLOW = "abcdefghijklmnopqrstuvwxyz";
+
+        char[] upCase = abcUP.toCharArray();
+        char[] lowCase = abcLOW.toCharArray();
+
+        // TODO: 10/11/2019 KALO ASCIInya di range huruf lowercase, pindahin jadi uppercase
+
+        System.out.println("UPPER ascii " + upCase[0] + " :" + (int) upCase[0]);
+        System.out.println("UPPER ascii " + upCase[25] + " :" + (int) upCase[25]);
+
+
+        System.out.println("LOWER ascii " + lowCase[0] + " :" + (int) lowCase[0]);
+        System.out.println("LOWER ascii " + lowCase[25] + " :" + (int) lowCase[25]);
+
+        String id_barang = "aweTYsSaA89op";
+        String id_baru = "";
+
+        System.out.println("IDNYA 1: " + id_barang);
+        int temp = 0;
+//        id_baru = id_barang.substring(0,j)+upCase[j]+id_barang.substring(j+1);
+        System.out.println("Penggunaan Looping: ");
+
+        // TODO: 10/11/2019 KALO PEMAKAIAN FOR SIH CITIL YAK :X
+        for (int i = 0; i < id_barang.length(); i++) {
+            if ((int) id_barang.charAt(i) < 123 && (int) id_barang.charAt(i) > 96) {
+                for (int j = 0; j < lowCase.length; j++) {
+                    if (id_barang.charAt(i) == lowCase[j]) {
+                        id_baru += upCase[j];
+                    }
+                }
+            } else {
+                id_baru += id_barang.charAt(i);
+            }
+        }
+        System.out.println("ID BARU: " + id_baru);
+        System.out.println("\n"+"Hasil Recrusive:");
+        System.out.println(recursiveUpper(id_barang));
+    }
+
+    private static String recursiveUpper(String obj) {
+        String abcUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abcLOW = "abcdefghijklmnopqrstuvwxyz";
+
+        char[] upCase = abcUP.toCharArray();
+        char[] lowCase = abcLOW.toCharArray();
+
+        String id_lama = obj;
+        String id_baru = "";
+
+        if (id_lama.length() == 0) {
+            return id_baru;
+        }else if (id_lama.length() == 1){
+            if ((int) id_lama.charAt(id_lama.length()-1) < 123 && (int) id_lama.charAt(id_lama.length()-1) > 96) {
+                for (int j = 0; j < lowCase.length; j++) {
+                    if (id_lama.charAt(id_lama.length()-1) == lowCase[j]) {
+                        id_baru += upCase[j];
+                    }
+                }
+                return id_baru;
+            }else {
+                id_baru += id_lama.charAt(id_lama.length()-1);
+
+                return id_baru;
+            }
+        }
+        else {
+            if ((int) id_lama.charAt(id_lama.length()-1) < 123 && (int) id_lama.charAt(id_lama.length()-1) > 96) {
+                for (int j = 0; j < lowCase.length; j++) {
+                    if (id_lama.charAt(id_lama.length()-1) == lowCase[j]) {
+                        id_baru = id_lama.substring(0,id_lama.length()-1);
+                        id_baru += upCase[j];
+                    }
+                }
+
+                recursiveUpper(id_baru.substring(0,id_baru.length() - 1));
+
+                System.out.println("id lama(if): "+ id_lama);
+                System.out.println("id baru(if): " + id_baru);
+                return id_baru;
+            }else {
+                id_baru = id_lama.substring(0,id_lama.length()-1);
+                id_baru += id_lama.charAt(id_lama.length()-1);
+
+                recursiveUpper(id_baru.substring(0,id_baru.length() - 1));
+
+                System.out.println("id lama(else): "+ id_lama);
+                System.out.println("id baru(else): " + id_baru);
+                return id_baru;
+            }
+        }
+//        return id_baru;
+    }
+}
